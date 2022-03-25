674a466f2d768b0c4cb56e7726b920fcc7bf3ca8
satya1807 <satyanalam9@gmail.com>
2022-03-25 16:52:26 +0530

    add: ERC721 minting contract

---
+++ contracts/Minter.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract Chip is ERC721 {
+    using SafeMath for uint256;
+
+    uint256 public totalSupply;
+    uint256 public constant mintingFee = 1e15; // 1e15 = 1 finney = 0.001 ether
+
+    constructor() ERC721("Chip", "CHIP") {
+        totalSupply = 0;
+    }
+
+    function mint(string calldata _uri) external payable returns (uint256) {
+        require(msg.value == mintingFee, "Incorrect amount paid");
+        require(bytes(_uri).length > 0, "Invalid uri");
+        totalSupply = totalSupply.add(1);
+        _mint(msg.sender, totalSupply);
+        return totalSupply;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "htpps://www.databaseuri.com/";
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
